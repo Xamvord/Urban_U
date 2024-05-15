1b390be7762f8b1002d9ffc33b4995ac38e9162b
Xamvord <logimp@gmail.com>
2024-05-15 23:26:56 +0300

    module 02, task 06

--- hw_m02_6.py
+++ hw_m02_6.py
@@ -1,10 +1,12 @@
 def generate_password(n):
     pairs = []
+    password = ''
     for i in range(1, 21):
         for j in range(i + 1, 21):
             if n % (i + j) == 0:
                 pairs.append((i, j))
-    password = ''.join([str(pair[0]) + str(pair[1]) for pair in pairs])
+    for pair in pairs:
+        password += str(pair[0]) + str(pair[1])
     return password
 
 n = int(input("Введите число от 3 до 20: "))
